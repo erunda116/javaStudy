5facd30cc3a7238efc875cd273f6686147b19710
MariyaY <sevwrestler@gmail.com>
2018-10-10 22:13:56 +0300

    first commit

---
+++ src/com/sqrt/Main.java
@@ -0,0 +1,14 @@
+package com.sqrt;
+/**
+ * This class multiply number to this number
+ * @author erunda
+ * */
+public class Main {
+
+    public static void main(String[] args) {
+        /** Argument of this function*/
+        int num = 10;
+        /** Output result */
+        System.out.println("square of number " + Math.pow(num,2));
+    }
+}
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.sqrt","l":"Main"}]
